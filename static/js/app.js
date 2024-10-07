6691b1e0252bce86946f69b3822c29c26afc8884
Kadeeza <dzawunor7@gmail.com>
2024-10-06 20:54:49 -0400

    backing up data

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,37 @@
+d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+  console.log(data);
+  let metadata = data.metadata
+  console.log(metadata);
+
+  let desiredata = metadata.find(mt => mt.id == 949 )
+  console.log(desiredata);
+});
+
+
 // Build the metadata panel
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let desiredata = metadata.find(mt => mt.id == sample);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select('#sample-metadata')
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+    Object.entries(desiredata).forEach(([key, value]) => {
+      // For each key-value pair, append a new <p> element with the text showing the pair
+      panel.append("p")
+           .text(`${key}: ${value}`);
 
+    });
   });
 }
 
@@ -25,20 +40,37 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
-
+    let samples = data.samples;
+   
     // Filter the samples for the object with the desired sample number
-
+    let desiredsample = samples.find(smp => smp.id == sample);
 
     // Get the otu_ids, otu_labels, and sample_values
-
-
+    let otu_ids = desiredsample.otu_ids;
+    let otu_labels = desiredsample.otu_labels;
+    let sample_values = desiredsample.sample_values;
+  
     // Build a Bubble Chart
+    let trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        color: otu_ids,
+        size: sample_values
+      }
+    };
 
+    let layout_bubble = {
+
+    };
+
+    let data = [trace1];
 
     // Render the Bubble Chart
-
-
+    Plotly.newPlot('bubble',data,layout_bubble);
+    
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
 
 
